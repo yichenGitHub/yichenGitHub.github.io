5ed2c12fefe1b8f472babd36adc213750c653ae8
Yi Chen <1906860J@student.tp.edu.sg>
2022-04-03 13:48:26 +0800

    feat: 1st iteration of portfolio website

---
+++ precache-manifest.4188dae23d58d4bde0a9fb07be010441.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2073ce7e6f546b99f44b0c1cfab6788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37e9522a50cd0b14aef1",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "abb8402701ef45c4b180",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "895c7ae9b04b15d0351a",
+    "url": "/static/css/main.e3308eab.chunk.css"
+  },
+  {
+    "revision": "e7630ac40d86e84cdd85",
+    "url": "/static/js/2.cb0b0597.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.cb0b0597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e9522a50cd0b14aef1",
+    "url": "/static/js/3.e9905139.chunk.js"
+  },
+  {
+    "revision": "abb8402701ef45c4b180",
+    "url": "/static/js/4.6838c13e.chunk.js"
+  },
+  {
+    "revision": "895c7ae9b04b15d0351a",
+    "url": "/static/js/main.d04004e5.chunk.js"
+  },
+  {
+    "revision": "1c907d0639121c89d960",
+    "url": "/static/js/runtime-main.1e6cfa65.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "32861dd2fc32951c01f361ff1295a535",
+    "url": "/static/media/geekoutLogo.32861dd2.jpg"
+  },
+  {
+    "revision": "c4d50fb7785957b1c24b01177fb3e0de",
+    "url": "/static/media/govtechLogo.c4d50fb7.jpg"
+  },
+  {
+    "revision": "8fcf8b61e4a39b50ce0606e4588475d7",
+    "url": "/static/media/knimeLogo.8fcf8b61.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3785f56d641263fccc03213dab1f578b",
+    "url": "/static/media/nvidiaLogo.3785f56d.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "38c108d634bc3fc29b4c4e1efcdb5bab",
+    "url": "/static/media/tpLogo.38c108d6.png"
+  }
+]);
